0d65fc0eac687ddf478d59ecf8935ad00a09186f
Student <Student@MB310-79>
2019-09-11 12:07:41 +0300

    commit

---
+++ SDP_task2_ERNESTAS_JUSKEVICIUS/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDP_task2_ERNESTAS_JUSKEVICIUS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
